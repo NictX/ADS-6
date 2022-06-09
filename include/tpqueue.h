459c3af4fc52218a72e13e30a254f860617fd78d
Nict <71667497+NictX@users.noreply.github.com>
2022-06-09 22:00:50 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -9,30 +9,29 @@ struct SYM {
 
 template<typename T, int size>
 class TPQueue {
-  private:
-    struct SYM arr[size];
-    int i = 0;
-  
-  public:
-    void push(SYM item) {
-      if (i < size) {
-        ++i;
-        for (int x = i - 1; x >= 0; x--) {
-          if (x && (item.prior <= arr[x - 1].prior)) {
-            arr[x] = arr[x - 1];
-          } else {
-            arr[x] = item;
-            break;
-          }
-        }
-      }
-    }
-  SYM pop() {
-    if (i > 0) {
-      return arr[--i];
-    } else {
-      throw "";
-    }
+ private:
+   struct SYM arr[size];
+   int i = 0;
+ public:
+   void push(SYM item) {
+     if (i < size) {
+       ++i;
+       for (int x = i - 1; x >= 0; x--) {
+         if (x && (item.prior <= arr[x - 1].prior)) {
+           arr[x] = arr[x - 1];
+         } else {
+           arr[x] = item;
+           break;
+         }
+       }
+     }
+   }
+   SYM pop() {
+     if (i > 0) {
+       return arr[--i];
+     } else {
+       throw "";
+   }
   }
 };
 
